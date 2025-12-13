eb9b6683695dc7865a874940ce1710ba902bce69
Joshua Chin <joshuarchin@gmail.com>
2025-12-13 14:38:29 -0500

    Run basic simulation

--- bin/main.ml
+++ bin/main.ml
@@ -1,97 +1,20 @@
 open! Base
+open! Stdio
 open! Hardcaml
-open! Signal
 
-let num_bits = 10;
-
-module I = struct
-    type 'a t = 
-        { clock: 'a
-        ; clear: 'a
-        ; data_in : 'a [@bits 8]
-        ; data_in_valid : 'a
-        }
-    [@@deriving hardcaml]
-end
-
-module O = struct
-    type 'a t = 
-        {
-            password: 'a [@bits num_bits]
-        }
-    [@@deriving hardcaml]
-end
+open! Advent_of_fpga_2025
 
 
-let create (scope: Scope.t) ({clock; clear; data_in; data_in_valid}: _ I.t): _ O.t
-    =
-    ignore scope;
-    let spec = Reg_spec.create ~clock ~clear () in
-    let open Always in
+let create_sim () =
+  let module Sim = Cyclesim.With_interface (Day01.I) (Day01.O) in
+  let scope = Scope.create ~auto_label_hierarchical_ports:true ~flatten_design:true () in
+  Sim.create (Day01.hierarchical scope)
 
-    (* Registers for parsing *)
-    let is_right = Variable.reg spec ~width:1 in
-    let hundreds = Variable.reg spec ~width:num_bits in
-    let tens = Variable.reg spec ~width:4 in 
-    let ones = Variable.reg spec ~width:4 in
-
-    (* Registers for logic *)
-    let dial =
-        let dial_width = 8 in
-        let clear_to_50 = Reg_spec.override ~clear_to:(of_int 50 ~width:dial_width) spec in
-        Variable.reg clear_to_50 ~width:dial_width
-    in
-    let password = Variable.reg spec ~width:num_bits in
-
-    compile [
-        when_ data_in_valid [
-            (* Parsing *)
-            when_ (data_in ==:. Char.to_int 'L') [
-                is_right <--. 0;
-            ];
-            when_ (data_in ==:. Char.to_int 'R') [
-                is_right <--. 1;
-            ];
-            when_ ((data_in >=:. Char.to_int '0' ) &: (data_in <=:. Char.to_int '9')) [
-                (
-                    let new_hundreds = hundreds.value *: (of_int 10 ~width:4) +: tens.value in
-                    hundreds <-- uresize new_hundreds num_bits
-                );
-                tens <-- ones.value;
-                (
-                    let d = data_in -:. (Char.to_int '0') in
-                    ones <-- uresize d 4
-                );
-            ];
-            (* Logic *)
-            when_ (data_in ==:. Char.to_int '\n') [
-                let rotation = tens.value *: (of_int 10 ~width:4) +: ones.value in
-                let new_password = password.value +: hundreds.value in
-                if_ is_right.value [
-                    let new_dial = dial.value +: rotation in
-                    if_ (new_dial >=:. 100) [
-                        dial <-- new_dial -:. 100;
-                        password <-- new_password +:. 1;
-                    ] [
-                        dial <-- rotation;
-                        password <-- new_password;
-                    ]
-                ] [
-                    let new_dial = dial.value -: rotation in
-                    let shift = (
-                        uresize (new_dial ==:. 0) num_bits -:
-                        uresize (dial.value ==:. 0) num_bits
-                    ) in
-                    if_ (new_dial <+. 0) [
-                        dial <-- new_dial +:. 100;
-                        password <-- new_password +: shift +:. 1;
-                    ] [
-                        dial <-- new_dial;
-                        password <-- new_password +: shift;
-                    ];
-                ];
-            ];
-        ];
-    ];
-
-    { password = password.value }
+let () =
+  let sim = create_sim () in
+  let inputs = Cyclesim.inputs sim in
+  let outputs = Cyclesim.outputs sim in
+  inputs.clear := Bits.vdd;
+  Cyclesim.cycle sim;
+  inputs.clear := Bits.gnd;
+  print_endline (Bits.to_string !(outputs.password))
---
+++ lib/day01.ml
@@ -0,0 +1,101 @@
+open! Base
+open! Hardcaml
+open! Signal
+
+let digit_bits = 4 (* 0-9, inclusive*)
+let dial_bits = 8 (* -99-99 OR 0-198, inclusive*)
+let output_bits = 20
+
+module I = struct
+    type 'a t = 
+        { clock: 'a
+        ; clear: 'a
+        ; data_in : 'a [@bits 8]
+        ; data_in_valid : 'a
+        }
+    [@@deriving hardcaml]
+end
+
+module O = struct
+    type 'a t = 
+        {
+            password: 'a [@bits output_bits]
+        }
+    [@@deriving hardcaml]
+end
+
+
+let create (scope: Scope.t) ({clock; clear; data_in; data_in_valid}: _ I.t): _ O.t
+    =
+    ignore scope;
+    let spec = Reg_spec.create ~clock ~clear () in
+    let open Always in
+
+    (* Registers for parsing *)
+    let%hw_var is_right = Variable.reg spec ~width:1 in
+    let%hw_var hundreds = Variable.reg spec ~width:output_bits in
+    let%hw_var tens = Variable.reg spec ~width:digit_bits in 
+    let%hw_var ones = Variable.reg spec ~width:digit_bits in
+
+    (* Registers for logic *)
+    let%hw_var dial =
+        let clear_to_50 = Reg_spec.override ~clear_to:(of_int 50 ~width:dial_bits) spec in
+        Variable.reg clear_to_50 ~width:dial_bits
+    in
+    let%hw_var password = Variable.reg spec ~width:output_bits in
+
+    compile [
+        when_ data_in_valid [
+            (* Parsing *)
+            when_ (data_in ==:. Char.to_int 'L') [
+                is_right <--. 0;
+            ];
+            when_ (data_in ==:. Char.to_int 'R') [
+                is_right <--. 1;
+            ];
+            when_ ((data_in >=:. Char.to_int '0' ) &: (data_in <=:. Char.to_int '9')) [
+                hundreds <-- uresize (hundreds.value *: (of_int 10 ~width:4)) output_bits
+                    +: uresize tens.value output_bits;
+                tens <-- ones.value;
+                (
+                    let d = data_in -:. (Char.to_int '0') in
+                    ones <-- uresize d digit_bits
+                );
+            ];
+            (* Logic *)
+            when_ (data_in ==:. Char.to_int '\n') [
+                let rotation = uresize (tens.value *: (of_int 10 ~width:4)) dial_bits
+                  +: uresize ones.value dial_bits in
+                let new_password = password.value +: hundreds.value in
+                if_ is_right.value [
+                    let new_dial = dial.value +: rotation in
+                    if_ (new_dial >=:. 100) [
+                        dial <-- new_dial -:. 100;
+                        password <-- new_password +:. 1;
+                    ] [
+                        dial <-- rotation;
+                        password <-- new_password;
+                    ]
+                ] [
+                    let new_dial = dial.value -: rotation in
+                    let shift = (
+                        uresize (new_dial ==:. 0) output_bits -:
+                        uresize (dial.value ==:. 0) output_bits
+                    ) in
+                    if_ (new_dial <+. 0) [
+                        dial <-- new_dial +:. 100;
+                        password <-- new_password +: shift +:. 1;
+                    ] [
+                        dial <-- new_dial;
+                        password <-- new_password +: shift;
+                    ];
+                ];
+            ];
+        ];
+    ];
+
+    { password = password.value }
+
+let hierarchical scope = 
+    let module Scoped = Hierarchy.In_scope (I) (O) in
+    Scoped.hierarchical ~scope ~name:"day_1" create
