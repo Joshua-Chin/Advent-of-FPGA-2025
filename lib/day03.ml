b0c837e25303655bde99f56ce9f293007271c9ed
Joshua Chin <joshuarchin@gmail.com>
2025-12-14 09:19:30 -0500

    format day 3

--- lib/day03.ml
+++ lib/day03.ml
@@ -45,11 +45,13 @@ let create (scope : Scope.t) ({ clock; clear; data_in; data_in_valid } : _ I.t)
       when_ data_in_valid
         [
           if_
-            ((data_in >:. Char.to_int '0') &: (data_in <=:. Char.to_int '9'))
+            (data_in >:. Char.to_int '0' &: (data_in <=:. Char.to_int '9'))
             [
               (let digit = uresize (data_in -:. Char.to_int '0') digit_bits in
                let new_joltages =
-                 List.map (pairwise (gnd :: joltage_list)) ~f:(fun (prev, curr) ->
+                 List.map
+                   (pairwise (gnd :: joltage_list))
+                   ~f:(fun (prev, curr) ->
                      let candidate = decimal_shift prev digit output_bits in
                      mux2 (candidate >: curr) candidate curr)
                  |> concat_lsb
@@ -57,14 +59,16 @@ let create (scope : Scope.t) ({ clock; clear; data_in; data_in_valid } : _ I.t)
                joltages <-- new_joltages);
             ]
             [
-              part1 <-- part1.value +: List.nth_exn joltage_list (part1_digits - 1);
-              part2 <-- part2.value +: List.nth_exn joltage_list (part2_digits - 1);
+              part1
+              <-- part1.value +: List.nth_exn joltage_list (part1_digits - 1);
+              part2
+              <-- part2.value +: List.nth_exn joltage_list (part2_digits - 1);
               joltages <-- zero (max_digits * output_bits);
             ];
         ];
     ];
 
-  { part1 = part1.value; part2 = part2.value };;
+  { part1 = part1.value; part2 = part2.value }
 
 let hierarchical scope =
   let module Scoped = Hierarchy.In_scope (I) (O) in
