8c162ab90b17ea21a2163d987d91e894a73a0bd3
Joshua Chin <joshuarchin@gmail.com>
2025-12-14 23:24:43 -0500

    Update day 4 to support arbitrary rows and cols

--- lib/day04.ml
+++ lib/day04.ml
@@ -3,6 +3,8 @@ open! Hardcaml
 open! Signal
 
 let output_bits = 16
+let rows = 150
+let cols = 150
 
 module I = struct
   type 'a t = {
@@ -26,7 +28,7 @@ module O = struct
 end
 
 module Config = struct
-  type 'a t = { rows : int; cols : int; initial_state : 'a Option.t }
+  type 'a t = { initial_state : 'a Option.t }
 end
 
 module States = struct
@@ -34,20 +36,32 @@ module States = struct
   [@@deriving sexp_of, compare ~localize, enumerate]
 end
 
-let create ~config:({ rows; cols; initial_state } : _ Config.t)
+let rec pairwise = function
+  | x :: (y :: _ as rest) -> (x, y) :: pairwise rest
+  | _ -> []
+
+let create ~config:({ initial_state } : _ Config.t)
     (scope : Scope.t) ({ clock; clear; finish; data_in; data_in_valid } : _ I.t)
     : _ O.t =
   ignore scope;
   let spec = Reg_spec.create ~clock ~clear () in
   let open Always in
   let sm = State_machine.create (module States) spec in
-  let%hw_var grid =
+  let grid =
     match initial_state with
     | Some state ->
-        Variable.reg
-          (Reg_spec.override ~clear_to:state spec)
-          ~width:(rows * cols)
-    | None -> Variable.reg spec ~width:(rows * cols)
+        let state_array = to_array state in
+        Array.init rows ~f:(fun row ->
+            Array.init cols ~f:(fun col ->
+                let idx = (row * cols) + col in
+                let clear_to =
+                  if idx < Array.length state_array then state_array.(idx)
+                  else gnd
+                in
+                Variable.reg (Reg_spec.override ~clear_to spec) ~width:1))
+    | None ->
+        Array.init rows ~f:(fun _ ->
+            Array.init cols ~f:(fun _ -> Variable.reg spec ~width:1))
   in
 
   let%hw_var part1 = Variable.reg spec ~width:output_bits in
@@ -55,22 +69,34 @@ let create ~config:({ rows; cols; initial_state } : _ Config.t)
   let%hw_var part2 = Variable.reg spec ~width:output_bits in
   let%hw_var part2_valid = Variable.reg spec ~width:1 in
 
+  let shift_row new_value =
+    Array.mapi grid.(0) ~f:(fun idx cell ->
+        cell <-- if idx > 0 then grid.(0).(idx - 1).value else new_value)
+    |> Array.to_list |> proc
+  in
+
+  let update_grid ~f =
+    Array.mapi grid ~f:(fun i row ->
+        Array.mapi row ~f:(fun j cell -> cell <-- f i j)
+        |> Array.to_list |> proc)
+    |> Array.to_list |> proc
+  in
+
   let processing_grid =
-    mux2 (sm.is Accepting_inputs) (zero (rows * cols)) grid.value
+    Array.map grid ~f:(fun row ->
+        Array.map row ~f:(fun reg -> mux2 (sm.is Processing) reg.value gnd))
   in
 
   let get_cell row col =
     if 0 <= row && row < rows && 0 <= col && col < cols then
-      let idx = (row * cols) + col in
-      Some processing_grid.:[idx, idx]
+      Some processing_grid.(row).(col)
     else None
   in
 
   let get_cell_exn row col =
-    if 0 <= row && row < rows && 0 <= col && col < cols then
-      let idx = (row * cols) + col in
-      processing_grid.:[idx, idx]
-    else raise (Invalid_argument "Index is out of bounds.")
+    match get_cell row col with
+    | Some output -> output
+    | None -> raise (Invalid_argument "Index is out of bounds.")
   in
 
   let get_neighbors row col =
@@ -100,11 +126,10 @@ let create ~config:({ rows; cols; initial_state } : _ Config.t)
   in
 
   let new_grid =
-    List.concat_map (List.range 0 rows) ~f:(fun row ->
-        List.map (List.range 0 cols) ~f:(fun col ->
+    Array.init rows ~f:(fun row ->
+        Array.init cols ~f:(fun col ->
             let cell = get_cell_exn row col in
             cell &: ~:(is_accessible row col)))
-    |> concat_lsb
   in
 
   let pipelined_popcount xs =
@@ -122,10 +147,14 @@ let create ~config:({ rows; cols; initial_state } : _ Config.t)
           sum_reg ))
   in
 
-  let depth, changed =
-    pipelined_popcount (split_msb ~part_width:1 (processing_grid ^: new_grid))
+  let changed_flatten =
+    Array.map2_exn processing_grid new_grid ~f:(fun row new_row ->
+        Array.map2_exn row new_row ~f:( ^: ))
+    |> Array.map ~f:Array.to_list |> Array.to_list |> List.concat
   in
-  let changed = uresize changed output_bits in
+
+  let depth, changed_count = pipelined_popcount changed_flatten in
+  let changed_count = uresize changed_count output_bits in
   let changed_valid = pipeline spec ~n:depth (sm.is Processing) in
 
   compile
@@ -136,23 +165,27 @@ let create ~config:({ rows; cols; initial_state } : _ Config.t)
             [
               when_ data_in_valid
                 [
+                  when_ (data_in ==:. Char.to_int '@') [ shift_row vdd ];
+                  when_ (data_in ==:. Char.to_int '.') [ shift_row gnd ];
                   when_
-                    (data_in ==:. Char.to_int '@')
-                    [ grid <-- vdd @: lsbs grid.value ];
-                  when_
-                    (data_in ==:. Char.to_int '.')
-                    [ grid <-- gnd @: lsbs grid.value ];
+                    (data_in ==:. Char.to_int '\n')
+                    [
+                      update_grid ~f:(fun row col ->
+                          if row > 0 then grid.(row - 1).(col).value else gnd);
+                    ];
                 ];
               when_ finish [ sm.set_next Processing ];
             ] );
           ( Processing,
             [
-              grid <-- new_grid;
+              update_grid ~f:(fun row col -> new_grid.(row).(col));
               when_
                 (part1_valid.value ==: gnd &: changed_valid)
-                [ part1 <-- changed; part1_valid <-- vdd ];
-              part2 <-- part2.value +: changed;
-              when_ (changed_valid &: (changed ==:. 0)) [ part2_valid <-- vdd ];
+                [ part1 <-- changed_count; part1_valid <-- vdd ];
+              part2 <-- part2.value +: changed_count;
+              when_
+                (changed_valid &: (changed_count ==:. 0))
+                [ part2_valid <-- vdd ];
             ] );
         ];
     ];
