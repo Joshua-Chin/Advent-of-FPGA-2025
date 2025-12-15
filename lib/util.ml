65874df6b3f97f0a82f42b64389be4b9dd11e842
Joshua Chin <joshuarchin@gmail.com>
2025-12-15 18:20:07 -0500

    Format utility library

--- lib/util.ml
+++ lib/util.ml
@@ -2,7 +2,6 @@ open! Base
 open! Hardcaml
 open! Signal
 
-
 let try_parse_to_digit current new_digit =
   Always.(
     when_
@@ -18,21 +17,18 @@ let rec pairwise = function
   | _ -> []
 
 let compute_multiply_shift divisor upper_bounds =
-    let open Z in
-    let divisor = of_int divisor in
-    let upper_bounds = of_int upper_bounds in
-    let x = ((upper_bounds + one) / divisor * divisor) - one in
+  let open Z in
+  let divisor = of_int divisor in
+  let upper_bounds = of_int upper_bounds in
+  let x = ((upper_bounds + one) / divisor * divisor) - one in
 
-    let test p =
-      let two_p = shift_left one p in
-      let k = rem (divisor - rem two_p divisor) divisor in
-      if gt two_p (x * k) then
-        Some (to_int ((two_p + k) / divisor), p)
-      else
-        None
-    in
-    let start = numbits upper_bounds in
-    let stop = Int.(+) start (Int.(+) (numbits divisor) 3) in
-    Sequence.range start stop
-      |> Sequence.find_map ~f:test
-      |> Option.value_exn
+  let test p =
+    let two_p = shift_left one p in
+    let k = rem (divisor - rem two_p divisor) divisor in
+    if gt two_p (x * k) then Some (to_int ((two_p + k) / divisor), p) else None
+  in
+  let start = numbits upper_bounds in
+  let stop = Int.( + ) start (Int.( + ) (numbits divisor) 3) in
+  Sequence.range start stop |> Sequence.find_map ~f:test |> Option.value_exn
+
+let bit_length x = Int.floor_log2 x + 1
